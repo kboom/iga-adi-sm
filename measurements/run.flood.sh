dc949a1f7c446c37f6db1a2fb74ac058eb8cc1e5
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-12-31 09:23:25 +0100

    Flood for terrain sim

--- measurements/run.flood.sh
+++ measurements/run.flood.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export IGA_ADI_SM_OPTS="-Xmx12000m" && mpirun -np 1 -host node13:4 ./iga-adi-sm --problem-size 768 --problem flood --steps 500 --delta 0.00001 --terrain-file /home/stud/ggurgul/iga-adi-sm/terrain/malopolskie.txt --terrain-x-offset 560000 --terrain-y-offset 180000 --terrain-scale 100 -w --store-file /home/stud/ggurgul/iga-mf-results/flood.768.500
+export IGA_ADI_SM_OPTS="-Xmx12000m" && mpirun -np 1 -host node13:10 ../dist/iga-adi-sm/bin/iga-adi-sm --problem-size 768 --problem flood --steps 500 --delta 0.00001 --terrain-file /home/stud/ggurgul/iga-adi-sm/terrain/malopolskie.txt --terrain-x-offset 560000 --terrain-y-offset 180000 --terrain-scale 100 -w --store-file /home/stud/ggurgul/iga-mf-results/flood.768.500
