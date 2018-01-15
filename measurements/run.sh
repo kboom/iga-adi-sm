fb18d3b00ee37835101a39da2ddf3acb4df1809d
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2018-01-15 13:44:25 +0100

    Displaying log4j

--- measurements/run.sh
+++ measurements/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export JAVA_OPTS="-Xms7G -Xmx7G" && ./KomPlasTechSolver/bin/KomPlasTechSolver --problem-size $1 --steps 1
+export JAVA_OPTS="-Xms8G -Xmx32G" && ../dist/iga-adi-sm/bin/iga-adi-sm --problem-size $1 --steps 1 --problem heat
